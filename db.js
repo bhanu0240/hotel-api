f6a02fc37b2e0ec9071798660e5db791211d1158
bhanu0240 <bhanureddy0240@gmail.com>
2026-01-04 11:52:53 +0530

    stopped passing deprecated params

--- db.js
+++ db.js
@@ -7,10 +7,7 @@ async function connectDB() {
   if (isConnected) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
     console.log("MongoDB connected");
   } catch (err) {
